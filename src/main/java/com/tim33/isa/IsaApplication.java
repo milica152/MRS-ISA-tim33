98b758884ecc518bc2a53b09a3de727c6c338c7e
Ancicii <ana.anchy.pjevic7@gmail.com>
2019-04-13 05:19:50 +0200

    Pocetna strana

--- src/main/java/com/tim33/isa/IsaApplication.java
+++ src/main/java/com/tim33/isa/IsaApplication.java
@@ -1,9 +1,11 @@
 package com.tim33.isa;
 
+import com.tim33.isa.controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"controller", "dto.filter", "model", "repository", "service"})
 public class IsaApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/tim33/isa/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.tim33.isa.controller;
+
+import com.tim33.isa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+
+public class UserController {
+
+    @Autowired
+    UserService employeeService;
+
+
+
+}
